65c3ed02d86b9d9c092e94287d695db3987d64c9
CookieTRY <167604609+CookieTRY@users.noreply.github.com>
2024-10-20 17:20:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/adminpanel')
-def opy():
-    return render_template('index.html')
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=1430, debug=True)
